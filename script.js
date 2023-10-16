7825c0d9973c76ef526fcb2e3a52a52a414a6597
cemilkb <cemilkoybasi98hotmail.com>
2023-10-16 21:31:56 +0300

    kitap verileri alıdnı

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Input
+
+const bookName = document.getElementById("book-name")
+const authorName = document.getElementById("author-name")
+const isRead = document.getElementById("is-read")
+const pages = document.getElementById("pages")
+const summary = document.getElementById("summary")
+
+// Create
+
+const bu = new Object()
+
+// Function
+
+function book(name, author, pages, read, summary) {
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.summary = summary
+}
+
+// Modal
 
 const userSettings = document.querySelector("#user-settings");
 const showUserSettings = document.querySelector(".user-settings");
@@ -14,7 +37,7 @@ showUserSettings.addEventListener("click", () => {
 });
 
 closeUserSettings.addEventListener("click", () => {
-    userSettings.close();
+  userSettings.close();
 });
 
 // Add Book modal
@@ -23,6 +46,10 @@ showBook.addEventListener("click", () => {
   addBook.showModal();
 });
 
-closeBook.addEventListener("click", () => {
+closeBook.addEventListener("click", (e) => {
+  e.preventDefault()
+
+  let  a = new book(bookName.value, authorName.value, pages.value, isRead.checked, summary.value)
+  bu[`${bookName.value}`] = a
   addBook.close();
 });
