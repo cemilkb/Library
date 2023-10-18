89f8e2b91875030453a56cacada6120dbad45b31
cemilkb <cemilkoybasi98hotmail.com>
2023-10-18 18:21:04 +0300

    veri aktarımı ve değişimi sağlandı local stprage veya başka bir  data base bağlanarak test edilmesi gerekli

--- script.js
+++ script.js
@@ -6,13 +6,24 @@ const isRead = document.getElementById("is-read")
 const pages = document.getElementById("pages")
 const summary = document.getElementById("summary")
 
+const settingsName = document.getElementById("settings-name")
+const settingsAuthor = document.getElementById("settings-author")
+const settingsIsRead = document.getElementById("settings-is-read")
+const settingsPages = document.getElementById("settings-pages")
+const settingsSummary = document.getElementById("settings-summary")
+
 // Get Main
 
 let main = document.querySelector("main")
 
 // Create
 
-const library = new Object()
+const library = {
+ "Lord Of The Rings": { name: 'Lord Of The Rings', author: 'J.R.R Tolkien', pages: '123', read: false, summary: 'NANANANANANANANANANANANANANANANAANANANANANAANANANANAN' },
+  "falan filan": { name: 'falan filan', author: 'J.R.R Tolkien', pages: '123', read: false, summary: 'deneme' },
+  "ölümlü dünya": { name: 'ölümlü dünya', author: 'J.R.R Tolkien', pages: '123', read: false, summary: 'kobik' },
+  "cinayet süsü": { name: 'cinayet süsü', author: 'J.R.R Tolkien', pages: '123', read: false, summary: 'en kobiğii' },
+}
 
 // Function
 
@@ -26,7 +37,7 @@ function book(name, author, pages, read, summary) {
 
 
 
-function makeCard(book) {
+function makeCard(name, author, pages, read, summary) {
 
   let div = document.createElement("div")
   let img = document.createElement("img")
@@ -40,17 +51,26 @@ function makeCard(book) {
   let icon2 = document.createElement("div")
 
   div.classList.add("card")
-  div.setAttribute("name", `${book.name}`)
   img.setAttribute("src", "assets/kullan.png")
-  p.textContent = `${book.summary}`
-  h1.textContent = `${book.name}:`
+  h1.textContent = `${name}:`
+  p.textContent = `${summary}`
   cardBottom.classList.add("card-bottom")
   icon1.innerHTML = `<iconify-icon icon="tabler:eye-filled" class="is-read"></iconify-icon>`
   pageAuthor.classList.add("page-author")
-  authorP.textContent = `${book.author}`
-  pageP.textContent = `${book.pages}`
+  authorP.textContent = `${author}`
+  pageP.textContent = `${pages}`
   icon2.innerHTML = `<iconify-icon icon="ant-design:setting-filled" class="card-settings"></iconify-icon>`
-  icon2.setAttribute("check",`${book.name}`)
+
+  icon2.addEventListener("click", () => {
+    settingsName.value = `${name}`
+    settingsAuthor.value = `${author}`
+    settingsPages.value = `${pages}`
+    settingsSummary.value = `${summary}`
+
+
+    bookSettings.showModal();
+
+  })
 
 
   div.appendChild(img)
@@ -76,6 +96,9 @@ const addBook = document.querySelector("#add-book");
 const showBook = document.querySelector(".add");
 const closeBook = document.querySelector("#close-book");
 
+const bookSettings = document.querySelector("#book-settings");
+const closeBookSettings = document.querySelector("#close-book-settings");
+
 // User Settings modal
 
 showUserSettings.addEventListener("click", () => {
@@ -94,21 +117,39 @@ showBook.addEventListener("click", () => {
 
 closeBook.addEventListener("click", (e) => {
   e.preventDefault()
+  let as = true
 
+  for (key in library) {
+    if (key == bookName.value) {
+      as = false
+    }
+  }
+
+  // Make book and add library
   let newBook = new book(bookName.value, authorName.value, pages.value, isRead.checked, summary.value)
-  library[`${bookName.value}`] = newBook
 
-  makeCard(newBook)
+  // Same book control area
+  if (as == true) {
+    library[`${bookName.value}`] = newBook
+    makeCard(library[`${bookName.value}`].name, library[`${bookName.value}`].author, library[`${bookName.value}`].pages, library[`${bookName.value}`].read, library[`${bookName.value}`].summary)
+  } else {
+    alert("NAPTIN YİĞEN SEN YAW AYNI ADLI BİR KİTAP DA VAR YİĞEN")
+  }
 
-  let buna = document.querySelector(`div[check = "${newBook.name}" ]`)
-
-  buna.addEventListener("click", ()=>{
-    library[`dangoz`] = library[`${newBook.name}`]
-    console.log(library[`dangoz`])
-   delete library[`${newBook.name}`]
-  })
-   
-  console.log(buna)
   addBook.close();
 });
+
+// Setting Book Modal
+
+closeBookSettings.addEventListener("click", (e) => {
+  e.preventDefault()
+  library[`${settingsName.value}`].author = `${settingsAuthor.value}`
+  library[`${settingsName.value}`].pages = `${settingsPages.value}`
+  library[`${settingsName.value}`].summary = `${settingsSummary.value}`
+})
+
+
+for (key in library) {
+  makeCard(library[`${key}`].name, library[`${key}`].author, library[`${key}`].pages, library[`${key}`].read, library[`${key}`].summary)
+}
 
